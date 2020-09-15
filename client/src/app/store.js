9dd37b9c53dae474fda366b3f638634268c92d83
Andrew Chon <andrewdchon@gmail.com>
2020-09-15 16:30:18 -0500

    remove files

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import databaseReducer from '../slices/databaseSlice';
 
 export default configureStore({
 	reducer: {
-		counter: counterReducer,
 		database: databaseReducer,
 	},
 });
