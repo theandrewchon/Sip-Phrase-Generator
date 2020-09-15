89e610af02349ce6e8bb6fdcf98fa8c2b8d4f256
Andrew Chon <andrewdchon@gmail.com>
2020-09-15 16:37:02 -0500

    cors

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
-
 const express = require('express');
-
+const cors = require('cors')
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT;
 
 // Define middleware here
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
