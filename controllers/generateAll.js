4ee165ba3ade9435a77df99b14c4abeb3a3fcedc
Andrew Chon <andrewdchon@gmail.com>
2020-09-29 13:27:28 -0500

    error handling

--- controllers/generateAll.js
+++ controllers/generateAll.js
@@ -1,47 +1,53 @@
 const db = require('../models');
+const _ = require('lodash')
 
 const removePunctuation = (string) => {
   let regex = /[!"#$%&*+,./:;<=>?@[\]^_`{|}~]/g;
   return string.replace(regex, '');
 };
+const fixQuotes = (string) => {
+  return string.replace(/[\u2018\u2019]/g, "'")
+}
 
 
 module.exports = {
   generate: async function (req, res) {
     const { lang, queries } = req.query
+    if (queries.length === 0) {
+      return res.json({ empty: emptyArray, sentences: sentenceArray });
+    }
     const database = await db.Sentences.find({})
     let emptyArray = [];
     let sentencesObj = {};
-    const arr = removePunctuation(queries.toLowerCase().trim()).split(' ');
+    const arr = _.compact(fixQuotes(removePunctuation(queries.toLowerCase().trim())).split(' '));
 
-    arr.forEach((element) => {
-      let temp = database.filter((phrase) => {
-        if (lang === 'english') {
-          //Filter english words
-          if (
-            new RegExp(`\\b${element.toLowerCase()}\\b`).test(
-              phrase.english.toLowerCase()
-            )
-          ) {
-            return true;
-          } else {
-            return false;
+    if (arr.length) {
+      arr.forEach((element) => {
+        let temp = database.filter((phrase) => {
+          if (lang === 'english') {
+            const englishArr = _.compact(fixQuotes(removePunctuation(phrase.english.toLowerCase())).split(' '))
+            //Filter english words
+            if (englishArr.includes(element)) {
+              return true;
+            } else {
+              return false;
+            }
+          } else if (lang === 'korean') {
+            //Filter Korean words
+            let newStr = removePunctuation(phrase.korean);
+            let arr = newStr.split(' ');
+            return arr.includes(element);
           }
-        } else if (lang === 'korean') {
-          //Filter Korean words
-          let newStr = removePunctuation(phrase.korean);
-          let arr = newStr.split(' ');
-          return arr.includes(element);
-        }
 
-        //Filter Korean words
+          //Filter Korean words
+        });
+        if (temp.length) {
+          Object.assign(sentencesObj, { [element]: temp })
+        } else {
+          emptyArray.push(element);
+        }
       });
-      if (temp.length) {
-        Object.assign(sentencesObj, { [element]: temp })
-      } else {
-        emptyArray.push(element);
-      }
-    });
-    res.json({ empty: emptyArray, sentences: sentencesObj });
+    }
+    return res.json({ empty: emptyArray, sentences: sentencesObj });
   }
 }
